64bc3a975986af6666876a708e79d73b5d5065c7
hamze <hamzeahmedx@gmail.com>
2017-08-10 14:59:10 +0000

    Waa La Update gareeyay

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,21 +9,27 @@ class PagesController < ApplicationController
   
   def new 
     
-    @pages = Page.new
+    @page = Page.new
      
   end
   def create
     #render plain: params[:page].inspect
-    @page = Page.new(page_prams)
-    @page.save
-    redirect_to pages_show(@page)
+    @page = Page.new(page_params)
+    if @page.save
+      flash[:notice] = "Article was Sucessfuly Created "
+      redirect_to page_path(@page)
+    else
+      render 'new'
+    end
+    #@page.save
+    #
   end
   
   def show
-    
+    @page =  Page.find(params[:id])
   end
   private 
-  def page_prams
+  def page_params
     params.require(:page).permit(:title , :description)
   end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,4 @@
 class Page < ActiveRecord::Base
-  
+  validates :title , presence: true , length: {minimum: 3, maximum: 40}
+  validates :description , presence: true , length: {minimum: 5, maximum: 400}
 end
